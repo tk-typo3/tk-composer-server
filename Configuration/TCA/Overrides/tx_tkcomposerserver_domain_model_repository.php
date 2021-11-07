f2e4f34675cd9da657fbd0855b1a3d767d71951f
Timon Kreis <mail@timonkreis.de>
2021-11-07 21:23:00 +0100

    Obsolete copyright removed

--- Configuration/TCA/Overrides/tx_tkcomposerserver_domain_model_repository.php
+++ Configuration/TCA/Overrides/tx_tkcomposerserver_domain_model_repository.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * @author Timon Kreis <mail@timonkreis.de>
- * @copyright by Timon Kreis - All rights reserved
  * @license https://www.gnu.org/licenses/gpl-3.0.de.html
  */
 declare(strict_types = 1);
